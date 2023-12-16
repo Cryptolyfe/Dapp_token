934db2e03cc747c813a3300e0e874431c3308203
cryptolyfe <jackalhandz@protonmail.com>
2023-12-15 17:07:10 -0800

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
@@ -8,10 +8,14 @@ contract Token {
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
 
+mapping(address => uint256) public balanceOf;
+
+
 	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
+		balanceOf[msg.sender] = totalSupply;
 
 	}
 }
