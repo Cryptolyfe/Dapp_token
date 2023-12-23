b9ffb398de782e3c0cb5de5c38a20f50e40f9385
cryptolyfe <jackalhandz@protonmail.com>
2023-12-23 10:50:52 -0800

    7. Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,4 @@
- pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
@@ -27,7 +27,6 @@ contract Token {
 		string memory _name, 
 		string memory _symbol, 
 		uint256 _totalSupply
-
 	) {
 		name = _name;
 		symbol = _symbol;
@@ -41,16 +40,25 @@ contract Token {
 		returns (bool success) 
 
 	{
-
 		require(balanceOf[msg.sender] >= _value);
-		require(_to != address(0));
 
-		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-		balanceOf[_to] = balanceOf[_to] + _value;
 
-		emit Transfer(msg.sender, _to, _value);
+		_transfer(msg.sender, _to, _value);
 
 		return true;
+	}
+
+	function _transfer(
+		address _from,
+		address _to,
+		uint256 _value
+	) internal {
+		require(_to != address(0));
+
+		balanceOf[_from] = balanceOf[_from] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		emit Transfer(_from, _to, _value);		
 	}
 
 	function approve(address _spender, uint256 _value) 
@@ -65,5 +73,29 @@ contract Token {
 		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
+
+	function transferFrom(
+		address _from, 
+		address _to, 
+		uint256 _value
+	)
+		public
+		returns (bool success)
+
+	{
+
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+
+
+		allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+		_transfer(_from, _to, _value);
+
+		return true;
+
+
+	}
+
 }
   
--- test/Token.js
+++ test/Token.js
@@ -35,7 +35,7 @@ describe('Token', () => {
 		})
 
 		it('has correct decimals', async () => {
-			expect(await token.decimals()).to.equal('18')
+			expect(await token.decimals()).to.equal(decimals)
 		})
 
 		it('has correct total supply', async () => {
@@ -51,28 +51,29 @@ describe('Token', () => {
 		let amount, transaction, result
 
 		describe('Success', () => {
-					beforeEach(async () => {
-			amount = tokens(100)
-			transaction = await token.connect(deployer).transfer(receiver.address, amount)
-			result = await transaction.wait()
+
+			beforeEach(async () => {
+				amount = tokens(100)
+				transaction = await token.connect(deployer).transfer(receiver.address, amount)
+				result = await transaction.wait()
+			})
+
+			it('Transfers token balances', async () => {
+
+				expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+				expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			})
+
+			it('emits a Transfer event', async () => {
+				const event = result.events[0]
+				expect(event.event).to.equal('Transfer')
+
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+			})
 		})
-
-		it('Transfers token balances', async () => {
-
-			expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
-			expect(await token.balanceOf(receiver.address)).to.equal(amount)
-		})
-
-		it('Emits a Transfer event', async () => {
-			const event = result.events[0]
-			expect(event.event).to.equal('Transfer')
-
-			const args = event.args
-			expect(args.from).to.equal(deployer.address)
-			expect(args.to).to.equal(receiver.address)
-			expect(args.value).to.equal(amount)
-		})
-	})
 
 		describe('Failure', () => {
 			it('rejects insufficient balances', async () => {
@@ -89,6 +90,7 @@ describe('Token', () => {
 	})
 
 	describe('Approving Tokens', () => {
+		let amount, transaction, result
 
 		beforeEach(async () => {
 			amount = tokens(100)
@@ -102,23 +104,70 @@ describe('Token', () => {
 				expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
 			})
 
-		it('Emits a Approval event', async () => {
-			const event = result.events[0]
-			expect(event.event).to.equal('Approval')
+			it('emits a Approval event', async () => {
+				const event = result.events[0]
+				expect(event.event).to.equal('Approval')
 
-			const args = event.args
-			expect(args.owner).to.equal(deployer.address)
-			expect(args.spender).to.equal(exchange.address)
-			expect(args.value).to.equal(amount)
-		})			
+				const args = event.args
+				expect(args.owner).to.equal(deployer.address)
+				expect(args.spender).to.equal(exchange.address)
+				expect(args.value).to.equal(amount)
+			})			
 		})
 
 		describe('Failure', () => {
-			it('rejects invalid spenders', async () => {
+			it('rejects invalid spenders', async () => { 
 				await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
 			})
 
 		})
+
+	})
+
+	describe('Delegated Token Transfers', () => {
+		let amount, transaction, result
+
+		beforeEach(async () => {
+			amount = tokens(100)
+			transaction = await token.connect(deployer).approve(exchange.address, amount)
+			result = await transaction.wait()
+		})
+
+		describe('Success', () => {
+			beforeEach(async () => {
+				amount = tokens(100)
+				transaction = await token.connect(exchange).transferFrom(deployer.address, receiver.address, amount)
+				result = await transaction.wait()
+			})
+
+			it('transfers token balances', async () => {
+				expect(await token.balanceOf(deployer.address)).to.be.equal(ethers.utils.parseUnits('999900','ether'))
+				expect(await token.balanceOf(receiver.address)).to.be.equal(amount)
+			})
+
+			it('resets the allowance', async () => {
+				expect(await token.allowance(deployer.address, exchange.address)).to.be.equal(0)
+			})
+
+			it('emits a transfer event', async () => {
+				const event = result.events[0]
+				expect(event.event).to.equal('Transfer')
+
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)	
+			})		
+
+		})
+
+		describe('Failure', () => {
+			it('Rejects insufficient amounts', async () => {
+				const invalidAmount = tokens(100000000)
+				await expect(token.connect(exchange).transferFrom(deployer.address, receiver.address, invalidAmount)).to.be.reverted
+			})
+		})
+
 	})
 })
 
